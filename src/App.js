17e8ad678a0d62f52f7c13b36fedba81cfb295b7
Charly Palencia <ingcharlypalencia@gmail.com>
2022-02-20 18:48:05 -0500

    Include sidebar and banner test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react'
-import Card from './components/Card'
-
+import SideBar from './components/SideBar'
+import "@fontsource/roboto";
 
 const App = () => {
 
   return (
-    <div className='container mx-auto'>
-      <h1>Tailwind CSS</h1>
-      <h2>Incorporado con PostCss</h2>
-      <button className="btn btn-blue btn-blue:hover">Botón</button>
-      <div className="my-5 flex flex-wrap -mx-2">
-        <div className="w-full sm:w-1/2 lg:w-1/3 px-2 my-2">
-          <Card photo='https://picsum.photos/800/600' />
-        </div>
-        <div className="w-full sm:w-1/2 lg:w-1/3 px-2 my-2">
-          <Card photo='https://picsum.photos/800/600' />
-        </div>
-        <div className="w-full sm:w-1/2 lg:w-1/3 px-2 my-2">
-          <Card photo='https://picsum.photos/800/600' />
-        </div>
+    <div className='flex flex-row h-full bg-eerie'>
+      <SideBar/>
+      <div class="text-gray-700 h-screen w-full">
+      <div className="bg-test w-full h-3/4"></div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
-  purge: ['./src/**/**/*.{js,ts,jsx,tsx}'],
-  content: [],
+  content: ['./src/**/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      ...colors,
+      'eerie': '#1D1D1D',
+      'eerie-50': 'rgba(29, 29, 29, 0.5)',
+    },
     extend: {},
   },
   plugins: [],
